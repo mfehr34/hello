897d74aa924025978b9ccc53ad75d44fa1d5f44a
Mitchell Fehr <mfehr3@huskers.unl.edu>
2022-08-30 02:34:57 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ *Basic Hello World Program
+ */
+
+int main(int argc, char **argv) {
+    
+    printf("Hello, I'm Mitchell Fehr!\n");
+    printf("Major is Computer Science!\n");
+
+    return 0;
+}
